fb3c6c0db2c277603823b306167988d2f2698030
Autarn Sukonnakorn <one.at.once@gmail.com>
2022-02-09 11:14:38 +0700

    data avil

--- src/components/ri/data_availablity/input_data.vue
+++ src/components/ri/data_availablity/input_data.vue
@@ -40,11 +40,12 @@
       <q-separator />
 
       <q-tab-panels v-model="input.compareType" animated>
+        <!-- tab1 -->
         <q-tab-panel name="group">
           <div class="row justify-center q-pt-md">
             <div class="col-9">
               <div class="row items-center font-16">
-                <div class="col-4 fontW700 font-18">
+                <div class="col-3 fontW700 font-18" align="left">
                   Level of disaggregation
                 </div>
                 <div class="col-3">
@@ -79,12 +80,12 @@
                   use-chips
                   stack-label
                   dense
-                  style="width: 98%"
                   @input="showSelectedGroupList()"
                 />
               </div>
-              <div class="selectedBox q-pa-sm" align>
-                <div class="font-16"><b>Selected partner economy(ies)</b></div>
+              <div class="selectedBox q-pa-sm" align="left">
+                <div class="font-16 fontW700">Selected economy group</div>
+
                 <div class="q-pt-sm">
                   <div class="row">
                     <div
@@ -100,12 +101,12 @@
             </div>
           </div>
         </q-tab-panel>
-
+        <!-- tab2 -->
         <q-tab-panel name="specific">
           <div class="row justify-center q-pt-md">
-            <div class="col-9">
+            <div class="col-9" align="left">
               <div class="row items-center font-16">
-                <div class="col-4 fontW700 font-18">
+                <div class="col-3 fontW700 font-18">
                   Level of disaggregation
                 </div>
                 <div class="col-3">
@@ -125,11 +126,30 @@
                   />
                 </div>
               </div>
-              <div class="q-pt-md" align="left">
-                <div class="font-18 fontW700">Economy group</div>
+              <div class="q-pt-md">
+                <div class="font-18 fontW700">
+                  <b>Reporting economy</b>
+                </div>
+                <div>
+                  Select a reporting economy or a pre-selected group of interest
+                </div>
+              </div>
+              <div>
+                <q-select
+                  :options="countryOptions"
+                  v-model="input.reporting"
+                  dense
+                  use-chips
+                  style="width: 98%"
+                  @input="showSelectedReportList()"
+                />
+              </div>
+
+              <div class="q-pt-md">
+                <div class="font-18 fontW700">Partner economy</div>
                 <div class="font-14">
-                  Select two or more economies of interest or a pre-selected
-                  group
+                  Select one, many or a pre-selected group of partner economies
+                  of interest.
                 </div>
               </div>
               <div class="q-pb-lg">
@@ -140,12 +160,27 @@
                   use-chips
                   stack-label
                   dense
-                  style="width: 98%"
                   @input="showSelectedGroupList()"
                 />
               </div>
-              <div class="selectedBox q-pa-sm" align>
-                <div class="font-16"><b>Selected partner economy(ies)</b></div>
+              <div class="selectedBox q-pa-sm">
+                <div class="font-16 fontW700">Selected reporting economy</div>
+                <div class="font14" v-show="countryReportList.length == 0">
+                  &nbsp;
+                </div>
+                <div class="q-pt-sm">
+                  <div class="row">
+                    <div
+                      class="countryTag q-mr-sm q-px-md q-mb-sm"
+                      v-for="(item, index) in countryReportList"
+                      :key="index"
+                    >
+                      {{ item.label }}
+                    </div>
+                  </div>
+                </div>
+                <div class="font-16 fontW700">Selected partner economy</div>
+
                 <div class="q-pt-sm">
                   <div class="row">
                     <div
@@ -163,6 +198,7 @@
         </q-tab-panel>
       </q-tab-panels>
     </q-card>
+    <div class="q-pa-md"></div>
   </div>
 </template>
 
@@ -179,12 +215,30 @@ export default {
         compareType: "group",
         disaggregation: "pair",
         group: [],
-        partner: [],
         reporting: null,
       },
     };
   },
   methods: {
+    showSelectedReportList() {
+      this.countryReportList = [];
+
+      //   let iso = this.input.reporting.iso;
+      let isos = this.input.reporting.iso;
+      let tempList = this.countryGroupList(isos);
+
+      let test = [...new Set(tempList)];
+
+      test.forEach((x) => {
+        let temp = this.countryOptions.filter((y) => y.iso == x);
+        let inputCountry = {
+          label: temp[0].label,
+          iso: temp[0].iso,
+        };
+        this.countryReportList.push(inputCountry);
+      });
+      this.countryReportList.sort((a, b) => (a.label > b.label ? 1 : -1));
+    },
     showSelectedGroupList() {
       this.countryFullList = [];
       let countryPartyTemp = [];
@@ -226,7 +280,7 @@ export default {
 }
 .selectedBox {
   width: 100%;
-  height: 230px;
+  height: 220px;
   border: 1px dashed #c4c4c4;
 }
 .countryTag {
